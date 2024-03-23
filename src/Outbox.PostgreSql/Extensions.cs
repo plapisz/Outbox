343424b660d5ee7c3b4f559035a3ed2c06804ec8
Patryk Łapisz <plapisz@hotmail.com>
2024-03-23 15:38:13 +0100

    Added empty implementation for PostgreSql

---
+++ src/Outbox.PostgreSql/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Outbox.Configuration;
+using Outbox.PostgreSql.Repositories;
+using Outbox.Repositories;
+
+namespace Outbox.PostgreSql;
+
+public static class Extensions
+{
+    public static void PostgreSql(this IOutboxConfigurator configurator)
+    {
+        configurator.Services.AddScoped<IOutboxMessageRepository, PostgreSqlOutboxMessageRepository>();
+    }
+}
---
+++ src/Outbox.PostgreSql/Repositories/PostgreSqlOutboxMessageRepository.cs
@@ -0,0 +1,22 @@
+﻿using Outbox.Entities;
+using Outbox.Repositories;
+
+namespace Outbox.PostgreSql.Repositories;
+
+internal sealed class PostgreSqlOutboxMessageRepository : IOutboxMessageRepository
+{
+    public Task AddAsync(OutboxMessage outboxMessage)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(OutboxMessage outboxMessage)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<OutboxMessage>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
