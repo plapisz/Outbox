e6149cb837ab5f99c307efe076e0fab82f6fae22
Patryk Łapisz <plapisz@hotmail.com>
2024-04-23 17:09:01 +0200

    Added OutboxMessageFactory in tests

---
+++ tests/Outbox.Tests.Shared/Factories/OutboxMessageFactory.cs
@@ -0,0 +1,21 @@
+﻿using Outbox.Entities;
+using Outbox.Events;
+using System.Text.Json;
+
+namespace Outbox.Tests.Shared.Factories;
+
+public static class OutboxMessageFactory
+{
+    private static readonly JsonSerializerOptions SerializerOptions = new()
+    {
+        PropertyNameCaseInsensitive = true
+    };
+
+    public static OutboxMessage Create<TEvent>(TEvent @event) where TEvent : class, IOutboxEvent
+    {
+        var type = @event.GetType();
+        var data = JsonSerializer.Serialize(@event, type, SerializerOptions);
+
+        return new OutboxMessage(type.FullName, data, DateTime.Now);
+    }
+}
--- tests/Outbox.Tests.Unit/Processors/OutboxMessageProcessorTests.cs
+++ tests/Outbox.Tests.Unit/Processors/OutboxMessageProcessorTests.cs
@@ -9,6 +9,7 @@ using Outbox.RetryPolicy.Options;
 using Outbox.RetryPolicy.RemoveMessageStrategies.Resolvers;
 using Outbox.Serializers;
 using Outbox.Tests.Shared.Events;
+using Outbox.Tests.Shared.Factories;
 using Outbox.Time;
 using Xunit;
 
@@ -19,8 +20,8 @@ public class OutboxMessageProcessorTests
     [Fact]
     public async Task process_async_should_call_handler_and_delete_async()
     {
-        var message = GetMessage();
         var orderCreated = GetOrderCreatedEvent();
+        var message = OutboxMessageFactory.Create(orderCreated);
         _outboxMessageRepository.GetAllToProcessAsync().Returns(new List<OutboxMessage> { message });
         _outboxEventSerializer.Deserialize(message.Data, typeof(OrderCreated))
             .Returns(orderCreated);
@@ -62,11 +63,6 @@ public class OutboxMessageProcessorTests
             serviceProvider,
             new RetryPolicyOptions());
     }
-
-    private static OutboxMessage GetMessage()
-        => new("Outbox.Tests.Shared.Events.OrderCreated",
-            "{\"Id\":\"814b3ef3-8fcc-4af6-bf5d-9f6562b886de\",\"Number\":\"test-1\",\"CreatedAt\":\"2024-03-19T17:41:11.2140516+01:00\",\"IsConfirmed\":false,\"ConfirmedAt\":null,\"CustomerEmail\":\"johndoe@exmail.com\"",
-            DateTime.UtcNow);
 
     private static OrderCreated GetOrderCreatedEvent()
      => new(Guid.Parse("814b3ef3-8fcc-4af6-bf5d-9f6562b886de"),
