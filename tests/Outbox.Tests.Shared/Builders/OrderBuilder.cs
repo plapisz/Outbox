8e914f8d66c7fc61307aad1fbe7a7747b06c9fe6
Patryk Łapisz <plapisz@hotmail.com>
2024-04-27 16:14:02 +0200

    Added random options to OrderBuilder

--- tests/Outbox.Tests.Shared/Builders/OrderBuilder.cs
+++ tests/Outbox.Tests.Shared/Builders/OrderBuilder.cs
@@ -1,9 +1,11 @@
-﻿using Outbox.Tests.Shared.Types;
+﻿using Bogus;
+using Outbox.Tests.Shared.Types;
 
 namespace Outbox.Tests.Shared.Builders;
 
 public class OrderBuilder
 {
+    private readonly Faker _faker = new Faker();
     private Guid _id;
     private string _number;
     private DateTime _createdAt;
@@ -21,9 +23,23 @@ public class OrderBuilder
         return this;
     }
 
+    public OrderBuilder WithRandomId()
+    {
+        _id = _faker.Random.Guid();
+
+        return this;
+    }
+
     public OrderBuilder WithNumber(string number)
     {
         _number = number;
+
+        return this;
+    }
+
+    public OrderBuilder WithRandomNumber()
+    {
+        _number = _faker.Random.String2(8);
 
         return this;
     }
@@ -35,9 +51,23 @@ public class OrderBuilder
         return this;
     }
 
-    public OrderBuilder WithCustomerEmial(string customerEmail)
+    public OrderBuilder WithRandomCreatedAt()
+    {
+        _createdAt = _faker.Date.Past();
+
+        return this;
+    }
+
+    public OrderBuilder WithCustomerEmail(string customerEmail)
     {
         _customerEmail = customerEmail;
+
+        return this;
+    }
+
+    public OrderBuilder WithRandomCustomerEmail()
+    {
+        _customerEmail = _faker.Person.Email;
 
         return this;
     }
